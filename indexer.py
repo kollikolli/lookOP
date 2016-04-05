2b0ae7f219e90f2ba7fb55ecc77899e6d8a75695
vahevanoyan <vahe.vanoyan@gmail.com>
2016-04-04 20:51:41 -0600

    Score function implemented

--- indexer.py
+++ indexer.py
@@ -1,3 +1,4 @@
+from __future__ import division
 class LookOP(object):
     def __init__(self):
         self.terms={}
@@ -12,6 +13,7 @@ class LookOP(object):
     def index(self,fn):
         import csv
         self.terms={}
+        self.result={}
         self.documents={}
         curdoc=0
         
@@ -93,22 +95,45 @@ class LookOP(object):
 
                         self.terms[token]['tweets'].append(tweet)
 
-#        print self.terms['scotthamilton']
-#        print self.terms['kindle']
-
+        #print self.terms['Obama']
+        #print self.terms['kindle']
+    def calcualate_Score(self,term):
+        #I CHOSE -5 0 5 to calculate the final score so we can display, we need to discuss this
+        score=0
+        a=self.terms[term]
+        neg=self.terms[term]['neg']
+        pos=self.terms[term]['pos']
+        total=self.terms[term]['tf']
+        score=(-5*neg)+ (5*pos)
+        score=score/total
+        final = float("{0:.2f}".format(score))
+        return final
     def query(self, query):
             search_query = self.tokenize(query)
             for term in search_query:
-                print self.terms[term]
+                #print self.terms[term]
+                print self.calcualate_Score(term)
+                self.result[term]={}
+                self.result[term]["score"]=self.calcualate_Score(term)
+                self.result[term]["tweets"]=self.terms[term]["tweets"]
+                print self.result[term]
+
+
+
                 #TODO Add to a result list
 
             #TODO sort result list by cosine similarity to query
         #TODO transform result list to be usable by GUI
 
+    def cosine(self,searchText,text):
+
+        return
+
 def main(args):
     lookop=LookOP()
-#    lookop.index("testdata.manual.2009.06.14.csv")
-    lookop.index("training.1600000.processed.noemoticon.csv")
+    lookop.index("testdata.manual.2009.06.14.csv")
+    lookop.query("Obama")
+    #lookop.index("training.1600000.processed.noemoticon.csv")
 
 if __name__ == "__main__":
     import sys
